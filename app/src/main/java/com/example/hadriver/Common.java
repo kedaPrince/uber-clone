59cac486a50fe9b45aa94c74f554f97440604805
samplander <80094166+samplander@users.noreply.github.com>
2022-03-03 23:48:27 +0200

    Uber Clone App

---
+++ app/src/main/java/com/example/hadriver/Common.java
@@ -0,0 +1,33 @@
+package com.example.hadriver;
+
+import com.example.hadriver.Model.DriverInfoModel;
+
+public class Common {
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo" ;
+    public static final String DRIVERS_LOCATION_REFERENCES = "DriversLocation" ;
+    public static final String TOKEN_REFERENCE = "Token";
+    public static final String NOTI_TITTLE ="title" ;
+    public static final String NOTI_CONTENT ="body" ;
+    public static final String RIDER_PICKUP_LOCATION = "PickupLocation";
+    public static final String RIDER_KEY ="RiderKey";
+    public static final String REQUEST_DRIVER_TITLE = "RequestDriver";
+    public static final String REQUEST_DRIVER_DECLINE = "Decline";
+    public static final String DRIVER_KEY ="DriverKey" ;
+    public static final String RIDER_PICKUP_LOCATION_STRING = "PickupLocationString";
+    public static final String RIDER_DESTINATION_STRING ="DestinationLocationString" ;
+    public static final String RIDER_DESTINATION ="DestinationLocation" ;
+    public static final String RIDER_INFO ="Riders" ;
+    public static final String REQUEST_DRIVER_ACCEPT ="Accept" ;
+    public static final String TRIP_KEY = "TripKey";
+    public static final String TRIP_PICKUP_REF = "TripPickupLocation";
+    public static final double MIN_RANGE_PICKUP_IN_KM = 0.05;
+    public static final int WAIT_TIME_IN_MIN = 1;
+    public static final String TRIP_DESTINATION_LOCATION_REF ="TripDestinationLocation" ;
+    public static final String REQUEST_DRIVER_DECLINE_AND_REMOVE_TRIP = "DeclineAndRemoveTrip";
+    public static final String RIDER_COMPLETE_TRIP ="DriverCompleteTrip" ;
+
+
+    public  static DriverInfoModel currentUser;
+    public static String Trip = "Trips";
+
+}
---
+++ app/src/main/java/com/example/hadriver/Model/DriverInfoModel.java
@@ -0,0 +1,73 @@
+package com.example.hadriver.Model;
+
+public class DriverInfoModel {
+    private String firstName, lastName, phoneNumber, avatar, email, car, numberPlate;
+    private  double rating;
+
+    public DriverInfoModel() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+    public void setNumberPlate(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+}
